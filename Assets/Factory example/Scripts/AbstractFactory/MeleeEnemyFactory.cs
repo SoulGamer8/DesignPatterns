5e20c5743b4989309d9a1b6cca8e5e7a7d37ccca
anton <anton3103@outlook.com>
2025-09-26 13:57:43 +0100

    abstract factory

---
+++ Assets/Factory example/Scripts/AbstractFactory/MeleeEnemyFactory.cs
@@ -0,0 +1,37 @@
+using System;
+using Nevermindever.Enemy.Data;
+using Nevermindever.Enemy.Logic;
+using Nevermindever.Interface;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Nevermindever.Enemy.AbstractFactory {
+    public class MeleeEnemyFactory : IEnemyFactory {
+        private GameObject _prefab;
+        private Transform _playerTransform;
+        private IDamageable _playerIDamageable;
+        
+        public MeleeEnemyFactory(GameObject prefab, Transform playerTransform, IDamageable playerIDamageable) {
+            _prefab = prefab;
+            _playerTransform = playerTransform;
+            _playerIDamageable = playerIDamageable;
+        }
+        
+        public EnemyComponent CreateEnemy(EnemyData data, Vector3 position, Quaternion rotation) {
+            GameObject enemyGameObject  = Object.Instantiate(_prefab, position, rotation);
+            EnemyComponent enemyComponent = enemyGameObject.GetComponent<EnemyComponent>();
+            Health health = new Health(data.health);
+            if (data is MeleeEnemyData meleeData) {
+                MeleeEnemy enemy = new MeleeEnemy(meleeData.speed, meleeData.damage, meleeData.fireRate, meleeData.animator,
+                    _playerIDamageable, meleeData.escapeRange, meleeData.fireRange, meleeData.attackCooldown
+                );
+                enemyComponent.Initialize(enemy, health, _playerTransform, meleeData.color);
+            }
+            else {
+                throw new ArgumentException($"Wrong EnemyData type passed: {data.GetType()}");
+            }
+            return enemyComponent; 
+        }
+    }
+}
+ 
--- Assets/Factory example/Scripts/Enemy/Data/MeleeEnemyData.cs
+++ Assets/Factory example/Scripts/Enemy/Data/MeleeEnemyData.cs
@@ -4,6 +4,6 @@ namespace Nevermindever.Enemy.Data {
     [CreateAssetMenu(fileName = "MeleeEnemyData", menuName = "Scriptable Objects/MeleeEnemyData")]
     public class MeleeEnemyData : EnemyData {
         public float attackCooldown ;
-        // here you can add some paraterther  what only use in Melee enemy like shield 
+        // here you can add some things that only use in Melee enemy like shield
     }
 }
